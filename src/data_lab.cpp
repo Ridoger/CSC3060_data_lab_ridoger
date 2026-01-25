1b63f5056d14c96efa6256ccf1b14837b3b5f2b4
我骑着苟子 <99963122+Ridoger@users.noreply.github.com>
2026-01-25 15:55:41 +0000

    全部完成

--- src/data_lab.cpp
+++ src/data_lab.cpp
@@ -64,9 +64,11 @@ int32_t multiply(int32_t a, int32_t b) {
     int32_t result = 0;
     
     for (int32_t i = 0; i < 32; ++i) {
-        if (1 << i & b) {
-            result = add(result, a << i);
+        if (1 & b) {
+            result = add(result, a);
         }
+        a <<= 1;
+        b >>= 1;
     }
 
     return result;
@@ -74,11 +76,40 @@ int32_t multiply(int32_t a, int32_t b) {
 }
 
 int32_t divide(int32_t a, int32_t b) {
-    return a / b;
+
+    int32_t result = 0;
+
+    // Check for sign of quotient
+    // Operator priority: >>, &, ^; from high to low
+    int32_t minus = a >> 31 & 1 ^ b >> 31 & 1;
+
+    // Take absolute values
+    if (a >> 31 & 1) a = add(~a, 1);
+    if (b >> 31 & 1) b = add(~b, 1);
+    
+    for (int32_t i = 0; i < 32; ++i) {
+
+        // Check whether divisor's multiplier > dividend
+        // In case of overflow, we shrink the dividend instead of expanding the divisor
+        if (subtract(a >> subtract(31, i), b) >> 31 & 1 ^ 1) {
+            a = subtract(a, b << subtract(31, i));     // Reminder
+            result |= 1 << subtract(31, i);     // Add corresponding value to quotient
+        }
+
+    }
+
+    // Restore sign of quotient
+    minus && (result = add(~result, 1));
+
+    return result;
+
 }
 
 int32_t modulo(int32_t a, int32_t b) {
-    return a % b;
+    
+    // a mod b = a - (a / b) * b
+    return subtract(a, multiply(divide(a, b), b));
+
 }
 
 }  // namespace data_lab
