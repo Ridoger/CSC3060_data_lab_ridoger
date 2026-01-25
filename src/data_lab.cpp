e4bf9dd8614f62ebc5b46a0661cb34e232ef7136
我骑着苟子 <99963122+Ridoger@users.noreply.github.com>
2026-01-25 14:19:33 +0000

    前三个函数

--- src/data_lab.cpp
+++ src/data_lab.cpp
@@ -4,15 +4,73 @@
 namespace data_lab {
 
 int32_t add(int32_t a, int32_t b) {
-    return a + b;
+
+    int32_t sum, carry;
+
+    // Use and and xor to decompose addition
+    sum = a ^ b; 
+    carry = (a & b) << 1; 
+
+    // We need to calculate sum + carry
+    // This may generate a new carry, so we need to decompose again
+    // The longest propagation of carry in a int32_t addition is 31 times,
+    // so we repeat 31 times
+    a = sum; b = carry; sum = a ^ b; carry = (a & b) << 1; 
+    a = sum; b = carry; sum = a ^ b; carry = (a & b) << 1; 
+    a = sum; b = carry; sum = a ^ b; carry = (a & b) << 1; 
+    a = sum; b = carry; sum = a ^ b; carry = (a & b) << 1; 
+    a = sum; b = carry; sum = a ^ b; carry = (a & b) << 1; 
+    a = sum; b = carry; sum = a ^ b; carry = (a & b) << 1; 
+    a = sum; b = carry; sum = a ^ b; carry = (a & b) << 1; 
+    a = sum; b = carry; sum = a ^ b; carry = (a & b) << 1; 
+    a = sum; b = carry; sum = a ^ b; carry = (a & b) << 1; 
+    a = sum; b = carry; sum = a ^ b; carry = (a & b) << 1; 
+    a = sum; b = carry; sum = a ^ b; carry = (a & b) << 1; 
+    a = sum; b = carry; sum = a ^ b; carry = (a & b) << 1; 
+    a = sum; b = carry; sum = a ^ b; carry = (a & b) << 1; 
+    a = sum; b = carry; sum = a ^ b; carry = (a & b) << 1; 
+    a = sum; b = carry; sum = a ^ b; carry = (a & b) << 1; 
+    a = sum; b = carry; sum = a ^ b; carry = (a & b) << 1; 
+    a = sum; b = carry; sum = a ^ b; carry = (a & b) << 1; 
+    a = sum; b = carry; sum = a ^ b; carry = (a & b) << 1; 
+    a = sum; b = carry; sum = a ^ b; carry = (a & b) << 1; 
+    a = sum; b = carry; sum = a ^ b; carry = (a & b) << 1; 
+    a = sum; b = carry; sum = a ^ b; carry = (a & b) << 1; 
+    a = sum; b = carry; sum = a ^ b; carry = (a & b) << 1; 
+    a = sum; b = carry; sum = a ^ b; carry = (a & b) << 1; 
+    a = sum; b = carry; sum = a ^ b; carry = (a & b) << 1; 
+    a = sum; b = carry; sum = a ^ b; carry = (a & b) << 1; 
+    a = sum; b = carry; sum = a ^ b; carry = (a & b) << 1; 
+    a = sum; b = carry; sum = a ^ b; carry = (a & b) << 1; 
+    a = sum; b = carry; sum = a ^ b; carry = (a & b) << 1; 
+    a = sum; b = carry; sum = a ^ b; carry = (a & b) << 1; 
+    a = sum; b = carry; sum = a ^ b; carry = (a & b) << 1; 
+    a = sum; b = carry; sum = a ^ b; 
+
+    return sum;
+
 }
 
 int32_t subtract(int32_t a, int32_t b) {
-    return a - b;
+    
+    // a minus b is equivalent to a plus -b
+    // Use 2-complement to get -b
+    return add(a, add(~b, 1));
+
 }
 
 int32_t multiply(int32_t a, int32_t b) {
-    return a * b;
+
+    int32_t result = 0;
+    
+    for (int32_t i = 0; i < 32; ++i) {
+        if (1 << i & b) {
+            result = add(result, a << i);
+        }
+    }
+
+    return result;
+
 }
 
 int32_t divide(int32_t a, int32_t b) {
